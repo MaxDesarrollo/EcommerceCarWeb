b487c3b0434b1d59027a9bafb609f14954d1e0eb
MaxDesarrollo <maxdesarrollo@outlook.com>
2018-04-14 10:03:07 -0400

    Creacion del proyecto

---
+++ Udi.ecommerceCar/Udi.ecommerceCar.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Udi.ecommerceCar.API.MvcApplication" Language="C#" %>
---
+++ Udi.ecommerceCar/Udi.ecommerceCar.API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Udi.ecommerceCar.API.Startup))]
+namespace Udi.ecommerceCar.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
