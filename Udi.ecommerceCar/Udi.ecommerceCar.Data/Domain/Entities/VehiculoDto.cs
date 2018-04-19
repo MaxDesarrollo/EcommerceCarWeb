d80959d736684dcfda4ed6dba936df342604f3b1
MaxDesarrollo <maxdesarrollo@outlook.com>
2018-04-19 10:32:45 -0400

    Creacion Dtos

---
+++ Udi.ecommerceCar/Udi.ecommerceCar.Data/Domain/Entities/VehiculoDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udi.ecommerceCar.Data.Domain.Entities
+{
+    class VehiculoDto
+    {
+        private int InventarioVehiculoID { get; set; }
+        private int CantidadDisponible { get; set; }
+
+        private Double Precio { get; set; }
+
+        private string Color { get; set; }
+
+        DateTime Año { get; set; }
+
+    }
+}
