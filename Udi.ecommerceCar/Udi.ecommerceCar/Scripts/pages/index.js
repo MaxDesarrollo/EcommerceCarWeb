7c39d0da90f0f5c6612d4e8b7d4e71363a855a45
Raiden <raikun96@gmail.com>
2018-06-15 08:08:37 -0400

    Pagina principal ya sirve mostrando los productos, vehiculos o servicios que se habiliten para mostrarse en la Pagina Principal

--- Udi.ecommerceCar/Udi.ecommerceCar/Scripts/pages/index.js
+++ Udi.ecommerceCar/Udi.ecommerceCar/Scripts/pages/index.js
@@ -17,7 +17,6 @@
 }
 
 function obtenerVehiculosPrincipalesExitoso(res) {
-    console.log(res);
     if (res.Success) {
         var listaVehicleCardHtml = "";
         var vehiculosPrincipales = res.Data;
@@ -39,6 +38,51 @@ function obtenerVehiculosPrincipales() {
     solicitudAjax(url, obtenerVehiculosPrincipalesExitoso, datos, tipoDatos, tipo);
 }
 
+
+function getProductCardHtml(id, nombre, descripcion, urlImagen, tipoProducto, precio) {
+
+    if (precio === "Sin precio") {
+        precio = "";
+    } else precio = "Bs. " + precio;
+
+    var productCardHtml =
+        `<div class="product-card">
+            <div class ="product-card-image" style="background-image: url('Images/${tipoProducto}/${nombre}.jpg')"></div>
+
+            <div class="product-card-description">
+                <div class ="product-card-header">
+                    <span class ="product-card-title">
+                        <a href="Producto/Detalle/${id}">${nombre}</a>
+                    </span>
+                </div>
+
+                <div class ="product-card-detail">
+                    <div class ="product-card-price">
+                        ${precio}
+                    </div>
+                </div>
+            </div>
+        </div>`;
+
+    //<span class="product-card-description">${descripcion}</span>
+    //<img src="Images/${tipoProducto}/${nombre}.jpg" alt="${nombre}">
+
+    return productCardHtml;
+}
+
+function obtenerProductosPrincipalesExitoso(res) {
+    if (res.Success) {
+        var listaProductCardHtml = "";
+        var productosPrincipales = res.Data;
+
+        productosPrincipales.forEach(function (producto) {
+            listaProductCardHtml += getProductCardHtml(producto.ProductoId, producto.Nombre, producto.Descripcion, producto.Nombre, producto.TipoProducto, producto.Precio);
+        });
+
+        $("#productos .product-card-container").html(listaProductCardHtml);
+    }
+}
+
 function obtenerProductosPrincipales() {
     var url = "/Producto/ObtenerProductosPrincipales";
     var tipo = "GET";
@@ -48,6 +92,34 @@ function obtenerProductosPrincipales() {
     solicitudAjax(url, obtenerProductosPrincipalesExitoso, datos, tipoDatos, tipo);
 }
 
+function getServiceCardHtml(id, nombre, urlImagen) {
+    var serviceCardHtml =
+        `<div class="service-card">
+            <img src="Images/Servicios/${nombre}.jpg" alt="${nombre}" />
+
+            <div class="service-card-description">
+                <a href="Servicio/Detalle/${id}">${nombre}</a>
+            </div>
+        </div>`;
+
+    //<span class="product-card-description">${descripcion}</span>
+    //<img src="Images/${tipoProducto}/${nombre}.jpg" alt="${nombre}">
+
+    return serviceCardHtml;
+}
+
+function obtenerServiciosPrincipalesExitoso(res) {
+    if (res.Success) {
+        var listaServiceCardHtml = "";
+        var serviciosPrincipales = res.Data;
+
+        serviciosPrincipales.forEach(function (servicio) {
+            listaServiceCardHtml += getServiceCardHtml(servicio.ServicioId, servicio.TipoServicio, servicio.TipoServicio);
+        });
+
+        $("#servicios .service-card-container").html(listaServiceCardHtml);
+    }
+}
 
 function obtenerServiciosPrincipales() {
     var url = "/Servicio/ObtenerServiciosPrincipales";
@@ -61,6 +133,8 @@ function obtenerServiciosPrincipales() {
 
 function init() {
     obtenerVehiculosPrincipales();
+    obtenerProductosPrincipales();
+    obtenerServiciosPrincipales();
 }
 
 init();
